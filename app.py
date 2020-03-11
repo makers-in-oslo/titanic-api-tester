841bf0bc55b2b9597c8f797ba62388e9df3d562f
Bjornar Maelum <bjornar.maelum@ibm.com>
2020-03-11 14:33:03 +0100

    Added table with input data and changed naming of buttons

--- app.py
+++ app.py
@@ -6,88 +6,120 @@ import json
 import requests
 import os
 
+
 app = Flask(__name__)
 
 ## Get connection details for APIs
-titanic_staging_app_url = os.environ['TITANIC_STAGING_URL'] # change to your app name
-titanic_prod_app_url = os.environ['TITANIC_PROD_URL'] # change to your app name
+titanic_staging_app_url = os.environ["TITANIC_STAGING_URL"]  # change to your app name
+titanic_prod_app_url = os.environ["TITANIC_PROD_URL"]  # change to your app name
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
 
-@app.route('/production/', methods=['GET', 'POST'])
+    # sample data
+    data_sample = {
+        "pclass": 1,
+        "sex": "female",
+        "age": 4.0,
+        "sibsp": 1,
+        "parch": 0,
+        "fare": 7.25,
+        "embarked": "S",
+        "name": "Dr. D",
+        "ticket": "Some 1234",
+        "cabin": "KingPing",
+        "passengerid": 123,
+    }
+
+    return render_template("index.html", data=data_sample)
+
+
+@app.route("/production/", methods=["GET", "POST"])
 def production_api():
-    if request.method == 'POST':
-        
-        headers = {
-        'Content-Type': 'application/json'
-        }
+    if request.method == "POST":
+
+        headers = {"Content-Type": "application/json"}
 
         # sample data
         data = {
-                "pclass":1,  "sex":"female",
-                "age":4.0,
-                "sibsp":1,
-                "parch":0, "fare":7.25,
-                "embarked":"S",
-                "name":"Dr. D", 
-                "ticket": "Some 1234", 
-                "cabin": "KingPing",
-                "passengerid":123
-                }
+            "pclass": 1,
+            "sex": "female",
+            "age": 4.0,
+            "sibsp": 1,
+            "parch": 0,
+            "fare": 7.25,
+            "embarked": "S",
+            "name": "Dr. D",
+            "ticket": "Some 1234",
+            "cabin": "KingPing",
+            "passengerid": 123,
+        }
 
         data_to_api = json.dumps(data)
 
-        send_request_deployed = requests.request("POST", titanic_prod_app_url, headers=headers, data = data_to_api)
+        send_request_deployed = requests.request(
+            "POST", titanic_prod_app_url, headers=headers, data=data_to_api
+        )
         print(send_request_deployed)
         api_response = str(send_request_deployed.json())
-        #print(len(api_response))
+        # print(len(api_response))
         print("API response")
         print(send_request_deployed.json())
 
-        return render_template('production.html', api_status=send_request_deployed, api_response=api_response, data=data)
+        return render_template(
+            "production.html",
+            api_status=send_request_deployed,
+            api_response=api_response,
+            data=data,
+        )
 
 
-@app.route('/staging/', methods=['GET', 'POST'])
+@app.route("/staging/", methods=["GET", "POST"])
 def staging_api():
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        headers = {
-        'Content-Type': 'application/json'
-        }
+        headers = {"Content-Type": "application/json"}
 
         # sample data
         data = {
-                "pclass":1,
-                "sex":"female",
-                "age":4.0,
-                "sibsp":1,
-                "parch":0,
-                "fare":7.25,
-                "embarked":"S",
-                "name":"Dr. D", 
-                "ticket": "Some 1234", 
-                "cabin": "KingPing",
-                "passengerid":123
-                }
+            "pclass": 1,
+            "sex": "female",
+            "age": 4.0,
+            "sibsp": 1,
+            "parch": 0,
+            "fare": 7.25,
+            "embarked": "S",
+            "name": "Dr. D",
+            "ticket": "Some 1234",
+            "cabin": "KingPing",
+            "passengerid": 123,
+        }
 
         data_to_api = json.dumps(data)
 
-        send_request_deployed = requests.request("POST", titanic_staging_app_url, headers=headers, data = data_to_api)
+        send_request_deployed = requests.request(
+            "POST", titanic_staging_app_url, headers=headers, data=data_to_api
+        )
         print(send_request_deployed)
         api_response = str(send_request_deployed.json())
-        #print(len(api_response))
+        # print(len(api_response))
         print("API response")
         print(send_request_deployed.json())
 
-        return render_template('staging.html', api_status=send_request_deployed, api_response=api_response, data=data)
-        
-@app.route('/feature_engineering/')
+        return render_template(
+            "staging.html",
+            api_status=send_request_deployed,
+            api_response=api_response,
+            data=data,
+        )
+
+
+@app.route("/feature_engineering/")
 def feature_engineering():
-    print('Hei')
-    return redirect('/')
+    print("Hei")
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
